1956ee795898f2ac22265b172e5331ca5cd210d7
Boaztheostrich <81034456+Boaztheostrich@users.noreply.github.com>
2026-06-05 23:40:06 -0400

    Add BLE motor control page

---
+++ README.md
@@ -0,0 +1,5 @@
+# ZellerShoe BLE Motor Control
+
+Minimal Bluefy/Web Bluetooth control page and Arduino BLE sketch for PWM motor testing.
+
+Open the GitHub Pages URL in Bluefy on iPhone, connect to `ZellerShoe`, then use the slider, typed value, Start, and Stop controls.
---
+++ ble_motor_power_test/ble_motor_power_test.ino
@@ -0,0 +1,88 @@
+#include <ArduinoBLE.h>
+
+const int MOTOR_PIN = D7;
+
+// Custom BLE UUIDs. The web page uses these exact values.
+BLEService motorService("19b10000-e8f2-537e-4f6c-d104768a1214");
+BLECharacteristic powerCharacteristic(
+  "19b10001-e8f2-537e-4f6c-d104768a1214",
+  BLERead | BLEWrite,
+  20
+);
+
+int motorPower = 0;
+
+void setMotorPower(int value) {
+  motorPower = constrain(value, 0, 255);
+  analogWrite(MOTOR_PIN, motorPower);
+}
+
+int parsePowerValue(const unsigned char *data, int length) {
+  if (length == 1) {
+    return data[0];
+  }
+
+  char buffer[21];
+  int copyLength = min(length, 20);
+  memcpy(buffer, data, copyLength);
+  buffer[copyLength] = '\0';
+
+  return atoi(buffer);
+}
+
+void setup() {
+  pinMode(MOTOR_PIN, OUTPUT);
+  setMotorPower(0);
+
+  Serial.begin(115200);
+  delay(300);
+
+  if (!BLE.begin()) {
+    while (true) {
+      setMotorPower(0);
+      delay(1000);
+    }
+  }
+
+  BLE.setLocalName("ZellerShoe");
+  BLE.setDeviceName("ZellerShoe");
+  BLE.setAdvertisedService(motorService);
+
+  motorService.addCharacteristic(powerCharacteristic);
+  BLE.addService(motorService);
+
+  unsigned char initialValue[1] = {0};
+  powerCharacteristic.writeValue(initialValue, 1);
+
+  BLE.advertise();
+
+  Serial.println("ZellerShoe BLE motor power test");
+  Serial.println("Write 0-255 to motorPower characteristic");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    Serial.print("Connected: ");
+    Serial.println(central.address());
+
+    while (central.connected()) {
+      BLE.poll();
+
+      if (powerCharacteristic.written()) {
+        unsigned char data[20];
+        int length = powerCharacteristic.valueLength();
+        powerCharacteristic.readValue(data, length);
+
+        setMotorPower(parsePowerValue(data, length));
+
+        Serial.print("Motor power: ");
+        Serial.println(motorPower);
+      }
+    }
+
+    setMotorPower(0);
+    Serial.println("Disconnected, motor off");
+  }
+}
